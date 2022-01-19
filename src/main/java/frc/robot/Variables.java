2b9f4eaf27b677aba1d711409b51ef700fbb3e4a
ccans <55026208+ccans@users.noreply.github.com>
2022-01-19 11:24:49 -0600

    initial commit

---
+++ src/main/java/frc/robot/Variables.java
@@ -0,0 +1,36 @@
+package frc.robot;
+
+public class Variables {
+
+    // Motor Ports
+    public static final int frontLeftMotorPort = 1;
+    public static final int frontRightMotorPort = 4;
+    public static final int backLeftMotorPort = 2;
+    public static final int backRightMotorPort = 3;
+
+    public static final int conveyorMotorPort = 10;
+    public static final int feedMotorPort = 13;
+    public static final int shooterMotorTopPort = 5;
+    public static final int shooterMotorBottomPort = 6;
+
+    public static final int intakeMotorOnePort = 9;
+
+    public static final int controlMotorPort = 11;
+
+    // Index Speed Amounts
+    public static final double intakeMotorSpeed = -0.55;
+    public static final double conveyorIndexSpeed = 0.90;
+    public static final double feedIndexSpeed = 1.0;
+
+    // Climbing Motor Speeds
+
+    // Shooter and Control Panel speeds
+   
+
+    // Sensor Ports
+    
+
+    // Shooter Control Loop Voltages
+
+
+}
